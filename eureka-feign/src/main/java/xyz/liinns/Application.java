92e4e8feafc4bc2bb3fcc6a6fa6b9562166b7418
林乃胜 <876056609@qq.com>
2018-01-02 19:25:06 +0800

    Update Application.java

--- eureka-feign/src/main/java/xyz/liinns/Application.java
+++ eureka-feign/src/main/java/xyz/liinns/Application.java
@@ -5,11 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
-/**
- * Description:
- * Created by LiinNs on 2017-2-3 0003.
- * Copyright © 2014-2017 趣医网 www.quyiyuan.com 版权所有
- */
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
