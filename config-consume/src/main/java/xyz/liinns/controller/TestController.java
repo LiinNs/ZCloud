15d63fcc3bbe0d48c85433cec6a437eee193b0eb
林乃胜 <876056609@qq.com>
2018-01-02 19:28:25 +0800

    Update TestController.java

--- config-consume/src/main/java/xyz/liinns/controller/TestController.java
+++ config-consume/src/main/java/xyz/liinns/controller/TestController.java
@@ -5,11 +5,6 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * Description:
- * Created by LiinNs on 2017-2-4 0004.
- * Copyright © 2014-2017 趣医网 www.quyiyuan.com 版权所有
- */
 @RefreshScope
 @RestController
 class TestController {
