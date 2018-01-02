7a756e1cb7a0baffe02541a83a3c91ba16fd52d6
林乃胜 <876056609@qq.com>
2018-01-02 19:26:04 +0800

    Update ComputeClientHystrix.java

--- eureka-feign/src/main/java/xyz/liinns/service/impl/ComputeClientHystrix.java
+++ eureka-feign/src/main/java/xyz/liinns/service/impl/ComputeClientHystrix.java
@@ -4,11 +4,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestParam;
 import xyz.liinns.service.ComputeClient;
 
-/**
- * Description:
- * Created by LiinNs on 2017-2-4 0004.
- * Copyright © 2014-2017 趣医网 www.quyiyuan.com 版权所有
- */
 @Component
 public class ComputeClientHystrix implements ComputeClient {
     @Override
