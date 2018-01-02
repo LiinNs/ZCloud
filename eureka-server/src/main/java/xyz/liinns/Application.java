2161233c7f9d77cb2e7b27ac463886e196d19d21
林乃胜 <876056609@qq.com>
2018-01-02 19:22:50 +0800

    Update Application.java

--- eureka-server/src/main/java/xyz/liinns/Application.java
+++ eureka-server/src/main/java/xyz/liinns/Application.java
@@ -4,11 +4,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-/**
- * Description:
- * Created by LiinNs on 2017-2-3 0003.
- * Copyright © 2014-2017 趣医网 www.quyiyuan.com 版权所有
- */
 @EnableEurekaServer
 @SpringBootApplication
 public class Application {
