9977b32d8d9ba6104d633417522214abbd313c00
林乃胜 <876056609@qq.com>
2018-01-02 19:27:27 +0800

    Update Application.java

--- config-server/src/main/java/xyz/liinns/Application.java
+++ config-server/src/main/java/xyz/liinns/Application.java
@@ -4,11 +4,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-/**
- * Description:
- * Created by LiinNs on 2017-2-4 0004.
- * Copyright © 2014-2017 趣医网 www.quyiyuan.com 版权所有
- */
 @EnableConfigServer
 @SpringBootApplication
 public class Application {
