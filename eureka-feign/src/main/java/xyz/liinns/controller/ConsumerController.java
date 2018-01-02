f4185f0d4c0149ee2368c5ec42b22c0b48b40419
林乃胜 <876056609@qq.com>
2018-01-02 19:26:30 +0800

    Update ConsumerController.java

--- eureka-feign/src/main/java/xyz/liinns/controller/ConsumerController.java
+++ eureka-feign/src/main/java/xyz/liinns/controller/ConsumerController.java
@@ -6,11 +6,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import xyz.liinns.service.ComputeClient;
 
-/**
- * Description:
- * Created by LiinNs on 2017-2-3 0003.
- * Copyright © 2014-2017 趣医网 www.quyiyuan.com 版权所有
- */
 @RestController
 public class ConsumerController {
 
