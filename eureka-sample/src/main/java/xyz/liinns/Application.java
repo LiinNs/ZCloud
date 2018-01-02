a721537ebdee383950d38744a0fa080df2f9e276
林乃胜 <876056609@qq.com>
2018-01-02 19:23:18 +0800

    Update Application.java

--- eureka-sample/src/main/java/xyz/liinns/Application.java
+++ eureka-sample/src/main/java/xyz/liinns/Application.java
@@ -4,11 +4,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-/**
- * Description:
- * Created by LiinNs on 2017-2-3 0003.
- * Copyright © 2014-2017 趣医网 www.quyiyuan.com 版权所有
- */
 @SpringBootApplication
 @EnableDiscoveryClient
 public class Application {
