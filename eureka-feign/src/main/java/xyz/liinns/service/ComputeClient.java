985068cdbd3af1a436d584992bcee0df7056afcc
林乃胜 <876056609@qq.com>
2018-01-02 19:25:32 +0800

    Update ComputeClient.java

--- eureka-feign/src/main/java/xyz/liinns/service/ComputeClient.java
+++ eureka-feign/src/main/java/xyz/liinns/service/ComputeClient.java
@@ -6,11 +6,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import xyz.liinns.service.impl.ComputeClientHystrix;
 
-/**
- * Description:
- * Created by LiinNs on 2017-2-3 0003.
- * Copyright © 2014-2017 趣医网 www.quyiyuan.com 版权所有
- */
 @FeignClient(name = "COMPUTE-SERVICE", fallback = ComputeClientHystrix.class)
 public interface ComputeClient {
     @RequestMapping(method = RequestMethod.GET, value = "/add")
